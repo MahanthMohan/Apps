34f702785a3ff1bde370c51d8e5715bbae221ef0
Mahanth Mohan <mohan.mahanth@gmail.com>
2020-04-25 20:39:08 -0700

    add

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        android.widget.EditText First_No = findViewById(R.id.First_No);
+        android.widget.EditText Second_No = findViewById(R.id.Second_No);
+        android.widget.Button addbtn = findViewById(R.id.addbtn);
+        android.widget.Button subbtn = findViewById(R.id.subbtn);
+        android.widget.Button multbtn = findViewById(R.id.multbtn);
+        android.widget.Button dvdbtn = findViewById(R.id.dvdbtn);
+        android.widget.TextView output = findViewById(R.id.output);
+
+        if(First_No.getText().toString().length() == 0)
+        {
+            First_No.setText("0");
+        }
+
+        if(Second_No.getText().toString().length() == 0)
+        {
+            Second_No.setText("0");
+        }
+
+
+        addbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer num1 = Integer.parseInt(First_No.getText().toString());
+                Integer num2 = Integer.parseInt(Second_No.getText().toString());
+                Integer res = num1 + num2;
+                output.setText(String.valueOf(res));
+            }
+        });
+
+        subbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer num1 = Integer.parseInt(First_No.getText().toString());
+                Integer num2 = Integer.parseInt(Second_No.getText().toString());
+                Integer res = num1 - num2;
+                output.setText(String.valueOf(res));
+            }
+        });
+
+        multbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer num1 = Integer.parseInt(First_No.getText().toString());
+                Integer num2 = Integer.parseInt(Second_No.getText().toString());
+                Integer res = num1 * num2;
+                output.setText(String.valueOf(res));
+            }
+        });
+        dvdbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer num1 = Integer.parseInt(First_No.getText().toString());
+                Integer num2 = Integer.parseInt(Second_No.getText().toString());
+                if(num2 == 0)
+                {
+                    output.setText("Undefined");
+                }
+                else {
+                    Integer res = num1 / num2;
+                    output.setText(String.valueOf(res));
+                }
+            }
+        });
+
+
+
+
+
+
+
+    }
+}
