e7a07a71fd6afb553b04c7d01a5546d0e2466cdd
Mahanth Mohan <mohan.mahanth@gmail.com>
2020-05-05 22:21:43 -0700

    commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,13 +12,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        android.widget.EditText First_No = findViewById(R.id.First_No);
-        android.widget.EditText Second_No = findViewById(R.id.Second_No);
-        android.widget.Button addbtn = findViewById(R.id.addbtn);
-        android.widget.Button subbtn = findViewById(R.id.subbtn);
-        android.widget.Button multbtn = findViewById(R.id.multbtn);
-        android.widget.Button dvdbtn = findViewById(R.id.dvdbtn);
-        android.widget.TextView output = findViewById(R.id.output);
+        EditText First_No = findViewById(R.id.First_No);
+        EditText Second_No = findViewById(R.id.Second_No);
+        Button addbtn = findViewById(R.id.addbtn);
+        Button subbtn = findViewById(R.id.subbtn);
+        Button multbtn = findViewById(R.id.multbtn);
+        Button dvdbtn = findViewById(R.id.dvdbtn);
+        TextView output = findViewById(R.id.output);
 
         if(First_No.getText().toString().length() == 0)
         {
@@ -65,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Float num1 = Float.parseFloat(First_No.getText().toString());
                 Float num2 = Float.parseFloat(Second_No.getText().toString());
+            
                 if(num2 == 0)
                 {
                     output.setText("Undefined");
